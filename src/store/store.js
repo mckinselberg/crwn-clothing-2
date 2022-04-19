0bf914f940bf05f47cfdb8a595736d110eb3598d
Dan <dan@dankinsley.com>
2022-04-19 19:21:37 -0400

    Added reselect lib. Created memoized version of Category Selector deleted:    package-lock.json modified:   package.json modified:   src/store/categories/category.selector.js modified:   src/store/store.js modified:   yarn.lock

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,19 @@
 import { compose, createStore, applyMiddleware } from 'redux';
-import { logger } from 'redux-logger';
+// import { logger } from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
 
-const middlewares = [logger];
+const loggerMiddleware = (store) => (next) => (action) => {
+  if (!action.type) {
+    return next(action);
+  }
+  console.log('type: ', action.type);
+  console.log('payload: ', action.payload);
+  console.log('currentState: ', store.getState());
+  next(action);
+  console.log('next state: ', store.getState());
+}
+const middlewares = [loggerMiddleware];
 
 const composedEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
