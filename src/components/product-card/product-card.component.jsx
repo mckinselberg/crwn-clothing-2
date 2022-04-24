a1a3f059ecdb160e6e821c36a8dc948ba0ccf39f
Dan <dan@dankinsley.com>
2022-04-24 13:21:42 -0400

    modified:   src/components/product-card/product-card.component.jsx

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -2,16 +2,18 @@ import { useDispatch, useSelector } from "react-redux";
 
 import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 import { ProductCardContainer } from "./product-card.styles";
-
 import { CART_ACTION_TYPES } from "../../store/cart/cart.types";
+
 import { addItemToCart } from "../../store/cart/cart.selector.functions";
 import { selectCartItems, selectTotalItems } from "../../store/cart/cart.selector";
 
 const ProductCard = ({ product }) => {
-  const { name, price, imageUrl } = product;
   const dispatch = useDispatch();
+  const { name, price, imageUrl } = product;
+
   const cartItems = useSelector(selectCartItems);
   const totalItems = useSelector(selectTotalItems);
+
   const addProductToCart = (product) =>
     dispatch({
       type: CART_ACTION_TYPES.SET_CART_ITEMS,
