c466a43a38c6861c2a8f89c3e290eda40c585579
Dan <dan@dankinsley.com>
2022-05-30 09:20:06 -0400

    Implemented stripe CC modified:   .gitignore new file:   .netlify/edge-functions-import-map.json new file:   netlify/functions/create-payment-intent.js modified:   package.json modified:   src/components/button/button.component.jsx modified:   src/components/button/button.styles.jsx new file:   src/components/payment-form/payment-form.component.jsx new file:   src/components/payment-form/payment-form.style.jsx new file:   src/components/spinner/spinner.components.jsx new file:   src/components/spinner/spinner.styles.jsx modified:   src/index.js modified:   src/routes/checkout/checkout.jsx modified:   src/routes/checkout/checkout.styles.jsx new file:   src/utils/stripe/stripe.utils.js modified:   yarn.lock

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux"
-import { PersistGate } from 'redux-persist/integration/react'
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { Elements } from "@stripe/react-stripe-js";
 
 import App from "./App";
 import { store, persistor } from "./store/store";
+import { stripePromise } from "./utils/stripe/stripe.utils";
 
 import "./index.scss";
 import reportWebVitals from "./reportWebVitals";
@@ -15,7 +17,9 @@ render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
--- src/routes/checkout/checkout.jsx
+++ src/routes/checkout/checkout.jsx
@@ -1,7 +1,17 @@
 import { useSelector } from "react-redux";
-import { selectCartItems, selectCartTotal } from "../../store/cart/cart.selector";
+import {
+  selectCartItems,
+  selectCartTotal,
+} from "../../store/cart/cart.selector";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
-import { CheckoutContainer, CheckoutHeader, HeaderBlock, Total } from "./checkout.styles.jsx";
+import {
+  CheckoutContainer,
+  CheckoutHeader,
+  HeaderBlock,
+  Total,
+} from "./checkout.styles.jsx";
+
+import PaymentForm from "../../components/payment-form/payment-form.component";
 
 const Checkout = () => {
   const cartItems = useSelector(selectCartItems);
@@ -25,6 +35,7 @@ const Checkout = () => {
       <div className="cart-totals">
         <Total>Cart Total: ${cartTotal}</Total>
       </div>
+      <PaymentForm/>
     </CheckoutContainer>
   );
 };
